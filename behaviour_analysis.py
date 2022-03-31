519f003f5f056b36c2cbe32de7f3a92e88867a86
ephys rig 1 <-->
2022-03-31 14:34:01 +0100

    fixed date index issue

--- behaviour_analysis.py
+++ behaviour_analysis.py
@@ -22,7 +22,7 @@ class TDAnalysis:
         self.trial_data = pd.concat(self.trial_data,sort=False,axis=0)
         self.animals = animal_list
         self.anon_animals = [f'Animal {i}' for i in range(len(self.animals))]  # give animals anon labels
-        self.dates = list(self.trial_data.loc[self.animals].index.unique())
+        self.dates = self.trial_data.loc[self.animals].index.to_frame()['Date'].unique()
 
         # add datetime cols
         for col in self.trial_data.keys():
@@ -34,21 +34,24 @@ class TDAnalysis:
         stats_dict = dict()
         for animal in self.animals:
             stats_dict[animal] = dict()  # dictionary to hold daily stats
-            for date in self.dates:
-                statnames = ['Trials Done', 'NonViol Trials', 'Early Rate', 'Error Rate']
-                data_day = utils.filter_df(self.trial_data.loc[animal, date], filters)
-                trials_done_day = data_day.shape[0]
-                correct_trials = utils.filter_df(data_day, ['a1']).shape[0]
+            for date in self.trial_data.loc[animal].index.unique():
                 try:
-                    early_rate = utils.filter_df(data_day, ['a2']).shape[0] / trials_done_day
-                except ZeroDivisionError:
-                    early_rate = 1
-                try:
-                    error_rate = utils.filter_df(data_day, ['a0']).shape[0] / utils.filter_df(data_day, ['a3']).shape[0]
-                except ZeroDivisionError:
-                    error_rate = 1
-                stats_day = pd.DataFrame([[trials_done_day, correct_trials, early_rate, error_rate]], columns=statnames)
-                stats_dict[animal][date] = stats_day
+                    statnames = ['Trials Done', 'NonViol Trials', 'Early Rate', 'Error Rate']
+                    data_day = utils.filter_df(self.trial_data.loc[animal, date], filters)
+                    trials_done_day = data_day.shape[0]
+                    correct_trials = utils.filter_df(data_day, ['a1']).shape[0]
+                    try:
+                        early_rate = utils.filter_df(data_day, ['a2']).shape[0] / trials_done_day
+                    except ZeroDivisionError:
+                        early_rate = 1
+                    try:
+                        error_rate = utils.filter_df(data_day, ['a0']).shape[0] / utils.filter_df(data_day, ['a3']).shape[0]
+                    except ZeroDivisionError:
+                        error_rate = 1
+                    stats_day = pd.DataFrame([[trials_done_day, correct_trials, early_rate, error_rate]], columns=statnames)
+                    stats_dict[animal][date] = stats_day
+                except KeyError:
+                    print('date missing')
 
         fig, ax = plt.subplots(len(stats_day.columns), sharex=True)
         if plot:
@@ -78,12 +81,13 @@ class TDAnalysis:
 if __name__ == '__main__':
     datadir = r'C:\bonsai\data'
     animals = [
-                'DO42',
-                'DO43',
-                'DO37',
+                'DO45',
+                'DO46',
+                'DO47',
+                'DO48'
                ]
 
-    dates = ['22/10/2021', '28/10/2021']  # start/end date for analysis
+    dates = ['17/03/2022', '30/10/2022']  # start/end date for analysis
     plot_colours = plt.cm.jet(np.linspace(0,1,len(animals)))  # generate list of col ids for each animal
     td_obj = TDAnalysis(datadir,animals,dates)
     td_obj.day2day = td_obj.beh_daily(True)
